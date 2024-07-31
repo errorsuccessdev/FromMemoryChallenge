c3c6b0f1d91b23b875102663b3da5ce36aeae5a4
ann <159976396+annlikeswindows@users.noreply.github.com>
2024-07-31 08:14:00 -0500

    Implemented check for a predetermined number.

--- FromMemoryChallenge/FromMemoryChallenge.c
+++ FromMemoryChallenge/FromMemoryChallenge.c
@@ -1,31 +1,85 @@
 // Need OBS to hide windows when they are not the top level window
 
+// Don't forget to look at research comments!
+
 // Next: random number guesser game.
 
 #include <stdio.h>
 
-#define MAXLEN      500
-#define NUMELEMENTS 3
+// Research: Is this the bad stdlib?
+#include <stdlib.h>
+
+#define NOT !
+
+#define MAX_STRING_LEN  500
+#define GAME_MIN_NUMBER 1
+#define GAME_MAX_NUMBER 10
+
+// Why no bools in C? :( Because I need to git gud.
+
+int validateInput(char* input);
 
 int main()
 {
-    // Research: Is this valid C syntax or is it a C++ thing?
-    char myString[NUMELEMENTS][MAXLEN] = 
-        { "Twitch", "Viewers", "lowluds" };
-    for (int element = 0;
-         element < NUMELEMENTS;
-         element++)
+    char buffer[MAX_STRING_LEN];
+    int guess = 0;
+    int answer = 6;
+
+    // TODO: Way to exit the game
+    while (1)
     {
-        printf("%s, %s!\n", "Hello", myString[element]);
+        printf(
+            "Pick a number between %d and %d: ",
+            GAME_MIN_NUMBER,
+            GAME_MAX_NUMBER
+        );
+        fgets(
+            buffer,
+            MAX_STRING_LEN, 
+            stdin
+        );
+
+        // Research: Is there a better way to handle this?
+        buffer[MAX_STRING_LEN - 1] = 0;
+
+        guess = validateInput(buffer);
+        
+        // If their guess is not valid, prompt them again immediately
+        if (guess == 0)
+        {
+            continue;
+        }
+        printf(
+            "You guessed %d. ", 
+            guess
+        );
+        if (guess == answer)
+        {
+            printf("That's correct!\n");
+            break;
+        }
+        else if (guess < answer)
+        {
+            printf("The answer is higher.\n");
+        }
+        else
+        {
+            printf("The answer is lower.\n");
+        }
     }
+}
 
-    char buffer[MAXLEN];
+int validateInput(char* input)
+{
+    int inputAsInt = atoi(input);
 
-    printf("%s\n", "Say something to stdin! ");
-    fgets(buffer, MAXLEN, stdin);
-
-    // Research: Is there a better way to handle this?
-    buffer[MAXLEN - 1] = 0;
-
-    printf("%s\n", buffer);
+    if ((inputAsInt >= GAME_MIN_NUMBER) &&
+        (inputAsInt <= GAME_MAX_NUMBER))
+    {
+        return inputAsInt;
+    }
+    else
+    {
+        return 0; // Not valid for our purposes
+    }
 }
